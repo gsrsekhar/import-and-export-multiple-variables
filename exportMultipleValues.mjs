04684fa7abbf3333a38f81621069eecb6d5fac16
gsrs143 <sekhargondesi@gmail.com>
2022-04-08 07:33:51 +0000

    import and export multiple variables

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,11 @@
+const myArray = ["camel", 265, true, "5.6"];
+const bulb = { watts: 10, type: "LED" };
+
+const a = 2;
+function multiplyByFour(a) {
+  if (typeof a === typeof 3) {
+    return a * 4;
+  }
+}
+
+export { myArray, bulb, multiplyByFour };
